34c76131d74a09da12407695684a2a94fd4f6af9
mielmaswary <miel1983@gmail.com>
2021-08-23 14:48:16 +0300

    3

--- includes/sortAndFilterBtn.php
+++ includes/sortAndFilterBtn.php
@@ -1,8 +1,8 @@
 
-<div class="container-fluid flex-row justify-space-between">
+<div class="flex-row justify-space-between">
     <div class="flex-row justify-space-around">
-        <div class="btn-rect">מיין תוצאות</div>
-        <div class="btn-rect">סנן תוצאות</div>
+        <div class="sort-icon-btn">מיין תוצאות</div>
+        <div class="filer-icon-btn">סנן תוצאות</div>
     </div>
 
     <div class="btn-rect">מפה</div>

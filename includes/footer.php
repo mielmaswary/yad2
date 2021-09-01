4c5348aead4c1d9f4c9a3a9518169a5781ece3cc
mielmaswary <miel1983@gmail.com>
2021-09-01 15:34:10 +0300

    5

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,28 @@
 <div id="footer-container"class="footer container-full dark-gray-bg">
-    <div class="flex-row justify-space-around padding-4 container-fluid">
+    <div class="flex-row justify-space-around padding-4 container-fluid padding-15">
         <a href="">רכב</a>
         <a href="">נדלן</a>
         <a href="">יד שנייה</a>
         <a href="">דרושים</a>
     </div>
-    <div class="flex-row ">socials-logos</div>
-    <div class="container-center font-size-small font-weight-light text-align-center bottom-line padding-4">כל הזכויות שמורות לחברת קורל תל מפעילות לוח יד2 - לוח מודעות:<br> דרושים IL,
-         דירות להשכרה, בתים למכירה, בתים להשכרה, העברת בתים,<br> 
+    <div class="flex-row justify-space-around container-60 padding-15 ">
+        <div class="footer-social-logo apple-store-logo"></div>
+        <div class="footer-social-logo google-play-logo"></div>
+        <div class="footer-social-logo fb-logo"></div>
+        <div class="footer-social-logo youtube-logo"></div>
+
+    </div>
+    <div class="container-85 font-size-small font-weight-light text-align-center bottom-line word-break-keep-all padding-15">כל הזכויות שמורות לחברת קורל תל מפעילות לוח יד2 - לוח מודעות: דרושים IL,
+         דירות להשכרה, בתים למכירה, בתים להשכרה, העברת בתים, 
          הובלות אין לעשות שימוש בכל התכנים המופיעים בלוח יד2.</div>
-    <div>bottom-links</div>
-    <div>גרסה: pro</div>
+    <div  class="footer-menu flex-row flex-wrap justify-space-around container-85 padding-15 margin-sides-items-15 font-size-small">
+        <div >תקנון</div>
+        <div>הצהרת נגישות</div>
+        <div >מדיניות פרטיות</div>
+        <div>מפת האתר</div>
+        <div>צור קשר</div>
+        <div>נגישות</div>
+    </div>
+    <div class="container-half text-align-center padding-15">גרסה: pro</div>
 
 </div>

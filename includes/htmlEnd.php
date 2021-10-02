cde5bca2a7ffc1e57f38b1e48dbb53db166ddc7f
mielmaswary <miel1983@gmail.com>
2021-10-02 21:56:00 +0300

    8

--- includes/htmlEnd.php
+++ includes/htmlEnd.php
@@ -1,7 +1,7 @@
 
     
-    <script src="https://miel-yad2.herokuapp.com/js/search.js"></script>
-    <script src="https://miel-yad2.herokuapp.com/js/main.js"></script>
+    <!-- <script src="../js/search.js"></script> -->
+    <script src="./js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/jszip.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/xlsx.js"></script>
   </body>

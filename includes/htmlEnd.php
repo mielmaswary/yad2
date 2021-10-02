1692e1ac7a08257970ce4889faeef72648e2225e
mielmaswary <miel1983@gmail.com>
2021-10-02 21:58:48 +0300

    8

--- includes/htmlEnd.php
+++ includes/htmlEnd.php
@@ -1,6 +1,6 @@
 
     
-    <!-- <script src="../js/search.js"></script> -->
+    <script src="../js/search.js"></script>
     <script src="./js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/jszip.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/xlsx.js"></script>

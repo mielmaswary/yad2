bc1fd822b2d9719c0fbeee48e77400d4dc629261
mielmaswary <miel1983@gmail.com>
2021-10-03 17:41:49 +0300

    8

--- db/dbCon.php
+++ db/dbCon.php
@@ -1,50 +1,21 @@
 <?php
 
    
-// $connectionection=mysqli_connect("localhost","root", "");
-// if(mysqli_connect_errno())
-//      {
-//          echo "error!".mysqli_connect_error();
-//      }
-// $sql='USE yad2';
-// if(mysqli_query($connectionection,$sql))
-// {
-// //    echo "Using yad2. </br>";
-// }
-// else
-// {
-//     echo "Error ".mysqli_error($connectionection);
-// }
-?>
-
-
-
-<?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-
-// Connect to DB
-$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
+$connectionection=mysqli_connect("localhost","root", "");
 if(mysqli_connect_errno())
      {
          echo "error!".mysqli_connect_error();
      }
-    
-    
- $sql='USE heroku_9839d4a91e8167b';
- if(mysqli_query($connection,$sql))
- {
-      
- }
- else
- {
-     echo "Error!!!!! ".mysqli_error($connection);
- }
+$sql='USE yad2';
+if(mysqli_query($connectionection,$sql))
+{
+//    echo "Using yad2. </br>";
+}
+else
+{
+    echo "Error ".mysqli_error($connectionection);
+}
 ?>
+
+
+
---
+++ db/dbPordCon.php
@@ -0,0 +1,29 @@
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+if(mysqli_connect_errno())
+     {
+         echo "error!".mysqli_connect_error();
+     }
+    
+    
+ $sql='USE heroku_9839d4a91e8167b';
+ if(mysqli_query($connection,$sql))
+ {
+      
+ }
+ else
+ {
+     echo "Error!!!!! ".mysqli_error($connection);
+ }
+?>

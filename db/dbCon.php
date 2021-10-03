adac5d944581500856504c784c317ceb19e71a04
mielmaswary <miel1983@gmail.com>
2021-10-03 18:04:31 +0300

    8

--- db/dbCon.php
+++ db/dbCon.php
@@ -1,19 +1,19 @@
 <?php
 
    
-$connectionection=mysqli_connect("localhost","root", "");
+$connection=mysqli_connect("localhost","root", "");
 if(mysqli_connect_errno())
      {
          echo "error!".mysqli_connect_error();
      }
 $sql='USE yad2';
-if(mysqli_query($connectionection,$sql))
+if(mysqli_query($connection,$sql))
 {
 //    echo "Using yad2. </br>";
 }
 else
 {
-    echo "Error ".mysqli_error($connectionection);
+    echo "Error ".mysqli_error($connection);
 }
 ?>
 

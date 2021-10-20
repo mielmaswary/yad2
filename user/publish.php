42be561bb7d08501a847a84b93cb01ffd8d2c244
mielmaswary <miel1983@gmail.com>
2021-10-20 13:10:07 +0300

    publish page header added

--- user/publish.php
+++ user/publish.php
@@ -2,64 +2,88 @@
    require_once('../includes/htmlStart.php');
 ?>
 <div class="container-fluid  ">
-      <div id="publish-categories-btns" class="flex-row flex-wrap justify-space-evenly flex-margin-items-10">
+       <div class="flex-row justify-space-between margin-5">
+          <div class="yad2-logo icon-min-mid-size"></div>
+          <div class="flex-row align-center  ">
+             <div class="loginIcon icon-min-small-size "></div>
+             <div class="orange margin-4 white-space-nowrap ">מיאל מסוארי</div>
+             <div class="margin-4 white-space-nowrap">צור קשר</div>
+          </div>
+       </div>
+      <div class="font-size-large text-align-center margin-20 white-space-nowrap">אני רוצה לפרסם מודעה בלוח...</div>
+      <div id="publish-categories-btns" class="flex-row flex-wrap justify-space-evenly flex-margin-items-10 flex-padding-items-10 text-align-center">
+         
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5  apartmentsIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">נדל"ן</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
-             <div class="flex-5  carIcon background-size-70 "></div>
-             <div class="flex-1"></div>
+             <div class="flex-5  carIcon"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1 ">כלי רכב</div>
           </div>
 
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5  sofaIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">יד שנייה</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5  businnesIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">עסקים למכירה</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5 handyManIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">בעלי מקצוע</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5 buildingsIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">נדל"ן מקצועי</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5 cellphoneIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">סלולר</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5 furnitureIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">ריהוט</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5 tourismIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">תיירות ונופש</div>
           </div>
 
           <div class="flex-column width-40 mid-rect grey-border">
              <div class="flex-5 fationIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">אופנה וטיפוח</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border"> 
              <div class="flex-5 petIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">חיות מחמד </div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border"> 
-             <div class="flex-5 babyIcon background-size-40"></div>
-             <div class="flex-1"></div>
+             <div class="flex-5 babyIcon "></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">לתינוק ולילד</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border"> 
              <div class="flex-5 pencilsIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">מורים פרטיים</div>
           </div>
           <div class="flex-column width-40 mid-rect grey-border"> 
              <div class="flex-5 suitcaseIcon"></div>
-             <div class="flex-1"></div>
+             <div class="mid-fat-separator-line"></div>
+             <div class="flex-1">דרושים</div>
           </div>
       </div>
 </div>

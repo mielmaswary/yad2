cf4c4a1a43ab1e4181d15cd48c6dc81cd1da9ce1
mielmaswary <miel1983@gmail.com>
2021-11-07 19:48:09 +0200

    publish-3

---
+++ user/publish-includes/contactDetails.php
@@ -0,0 +1,8 @@
+<div class="padding-16-8 font-size-small">
+    <div>רגע לפני שמפרסמים את המודעה, נבדוק שפרטי הקשר נכונים</div>
+    <br>
+    <div class="margin-bottom-4">שם איש קשר*</div>
+    <input class="width-100 padding-16-8 margin-bottom-4 " type="text" name="contactName">
+    <div class="margin-bottom-4">טלפון איש קשר*</div>
+    <input class="width-100 padding-16-8 margin-bottom-4 " type="text" name="contactPhone">
+</div>
--- user/publish-includes/imagesAndVideo.php
+++ user/publish-includes/imagesAndVideo.php
@@ -1,12 +1,49 @@
-<div class="flex-row flex-wrap justify-space-around padding-16-8 font-size-small">
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
-     <div class="flex-square margin-5"></div>
+<div class="flex-row flex-wrap justify-space-around padding-16-8 font-size-small bright-grey">
+     <div class="margin-10 main-text-color">ידעת שמודעות עם תמונות ברורות מקבלות פי 10 יותר פניות?
+            לא להסס להעלות לפה תמונות (אפשר עד 10 + וידאו) ולהבליט את הצדדים הטובים ביותר של הנכס
+    </div>
+    
+    <div class="flex-square-dashed margin-5 flex-column justify-end videoIcon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת סרטון</div>
+     </div>
+     <div class="relative flex-square main-img-btn margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="absolute-top-center width-100 grey-bg white-space-nowrap text-align-center main-text-color padding-4"> תמונה ראשית</div>
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     
+</div>
 
+<div class="padding-16-8">
+     <div class="dark-thin-separator-line"></div>
+</div>
+
+<div class="flex-row flex-wrap justify-space-around padding-16-8 font-size-small bright-grey">
+
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
+     <div class="flex-square-dashed margin-5 flex-column justify-end plus-icon background-size-20 flex-column padding-3prec">
+          <div class="text-align-center ">העלאת תמונות</div>
+     </div>
 </div>

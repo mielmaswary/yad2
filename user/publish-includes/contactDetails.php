1331d3774546f782736b5af3e69baf0be3ad65ea
mielmaswary <miel1983@gmail.com>
2021-11-08 21:09:29 +0200

    publish-4

--- user/publish-includes/contactDetails.php
+++ user/publish-includes/contactDetails.php
@@ -5,4 +5,24 @@
     <input class="width-100 padding-16-8 margin-bottom-4 " type="text" name="contactName">
     <div class="margin-bottom-4">טלפון איש קשר*</div>
     <input class="width-100 padding-16-8 margin-bottom-4 " type="text" name="contactPhone">
+    <div class="separator-line"></div>
+    <div class="margin-bottom-4">איש קשר נוסף</div>
+    <input class="width-100 padding-16-8 margin-bottom-4 " type="text" name="contactName">
+    <div class="margin-bottom-4">טלפון נוסף</div>
+    <input class="width-100 padding-16-8 margin-bottom-4 " type="text" name="contactPhone">
+    <br><br>
+    <div class="padding-10">
+         <label class="container"> קראתי ואישרתי את<span class="orange">&nbsp;התקנון  </span>*
+               <input type="checkbox">
+               <span class="checkmark"></span>
+          </label>
+          <label class="container">אני רוצה שתשלחו לי עדכונים במייל
+               <input type="checkbox">
+               <span class="checkmark"></span>
+          </label>
+    </div>
+    <div class=" flex-row justify-end padding-10">
+        <div class="btn-ellips orange-bold orange-border font-size-13 ">המשך לבחירת מסלול</div>
+    </div>
+
 </div>

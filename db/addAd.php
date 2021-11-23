a625201ce28ef25cf853456caef61addd6f228d4
mielmaswary <miel1983@gmail.com>
2021-11-23 16:58:04 +0200

    adComplete

--- db/addAd.php
+++ db/addAd.php
@@ -1,13 +1,13 @@
 <?php
-
    require_once('dbCon.php');
-   function addAd($id, $apartmentType, $cityName, $cityType, $street, $adressNum, $floor, $totalsFloors, $pillars, $neighborhood, $area, $roomsNum, $size, $groundFloor, $bars, $pandor, $mmd, $furnished, $parking, $pets, $garage, $partners, $airConditioner, $invalids, $longTime, $porch, $renovated, $exclusivity, $price, $entryDate, $img1ID, $img2ID, $img3ID, $movieID, $awnerID){
+   function addAd($apartmentType,$cityName,$cityType,$street,$adressNum,$floor,$totalsFloors,$pillars,$neighborhood,$area,$roomsNum,$size,$groundFloor,$bars,$pandor,$mmd,$furnished,$parking,$pets,$garage,$partners,$airConditioner,$invalids,$longTime,$porch,$renovated,$exclusivity,$price,$entryDate,$img1,$img2,$img3,$movie,$awnerID){
        $connection=mysqli_connect("localhost","root", "");
        if(mysqli_connect_errno())
             {
                 echo "error!".mysqli_connect_error();
             }
        $sql='USE yad2';
+
        if(mysqli_query($connection,$sql))
        {
        //    echo "Using yad2. </br>";
@@ -16,18 +16,50 @@
        {
            echo "Error ".mysqli_error($connection);
        }
+       $sql="INSERT INTO `ads` (`apartmentType`, `cityName`, `cityType`, `street`, `adressNum`, `floor`, `totalsFloors`, `pillars`, `neighborhood`, `area`, `roomsNum`, `size`, `groundFloor`, `bars`, `pandor`, `mmd`, `furnished`, `parking`, `pets`, `garage`, `partners`, `airConditioner`, `invalids`, `longTime`, `porch`, `renovated`, `exclusivity`, `price`, `entryDate`, `img1`, `img2`, `img3`, `movie`, `awnerID`) VALUES ('$apartmentType', '$cityName', '$cityType', '$street', $adressNum, $floor, $totalsFloors, $pillars, '$neighborhood', '$area', $roomsNum, $size, $groundFloor, $bars, $pandor, $mmd, $furnished, $parking, $pets, $garage, $partners, $airConditioner, $invalids, $longTime, $porch, $renovated, $exclusivity, $price, '$entryDate', '$img1', '$img2', '$img3', '$movie', $awnerID);";   
 
-       $sql="INSERT INTO `ads` (`id`, `apartmentType`, `cityName`, `cityType`, `street`, `adressNum`, `floor`, `totalsFloors`, `pillars`, `neighborhood`, `area`, `roomsNum`, `size`, `groundFloor`, `bars`, `pandor`, `mmd`, `furnished`, `parking`, `pets`, `garage`, `partners`, `airConditioner`, `invalids`, `longTime`, `porch`, `renovated`, `exclusivity`, `price`, `entryDate`, `img1ID`, `img2ID`, `img3ID`, `movieID`, `awnerID`) VALUES ($id, '$apartmentType', '$cityName', '$cityType', '$street', $adressNum, $floor, $totalsFloors, $pillars, '$neighborhood', '$area', $roomsNum, $size, $groundFloor, $bars, $pandor, $mmd, $furnished, $parking, $pets, $garage, $partners, $airConditioner, $invalids, $longTime, $porch, $renovated, $exclusivity, $price, $entryDate, $img1ID, $img2ID, $img3ID, $movieID, $awnerID);";   
-         
        if(mysqli_query($connection,$sql))
        {
            
        }
        else
        {
+           
            echo "Error ".mysqli_error($connection);
        }
    }
 
-    
+
+
+//    function addAd($apartmentType){
+       
+//     $connection=mysqli_connect("localhost","root", "");
+//     if(mysqli_connect_errno())
+//          {
+//              echo "error!".mysqli_connect_error();
+//          }
+//     $sql='USE yad2';
+
+//     if(mysqli_query($connection,$sql))
+//     {
+//     //    echo "Using yad2. </br>";
+//     }
+//     else
+//     {
+//         echo "Error ".mysqli_error($connection);
+//     }
+
+//     $sql="INSERT INTO `ads` (`apartmentType`) VALUES ('$apartmentType');";   
+
+//     if(mysqli_query($connection,$sql))
+//     {
+//         echo 'sucsses!';
+//     }
+//     else
+//     {
+//         echo "Error ".mysqli_error($connection);
+//     }
+// }
+
+
 ?>
--- user/publish-includes/adComplete.php
+++ user/publish-includes/adComplete.php
@@ -1,20 +1,26 @@
 <?php
-   require_once('../../includes/htmlStart.php');
-?>
-<div class=" padding-16-8 font-size-small display-none">
-       <div class="flex-row justify-space-between margin-5">
-          <div class="yad2-logo icon-min-mid-size"></div>
-          <div class="flex-row align-center  ">
-             <div class="loginIcon icon-min-small-size "></div>
-             <div class="orange margin-4 white-space-nowrap ">מיאל מסוארי</div>
-             <div class="margin-4 white-space-nowrap">צור קשר</div>
-          </div>
-       </div>
-    <div>סיימת!</div>
 
-</div>
+    require_once('C:\wamp64\www\firstphp\yad2\includes\htmlStart.php');
+    require_once('C:\wamp64\www\firstphp\yad2\db\addAd.php');
+    if(isset($_GET['apartmentType'])){
+       addAd($_GET['apartmentType'],$_GET['cityName'],$_GET['cityType'],$_GET['street'],$_GET['adressNum'],$_GET['floor'],$_GET['totalsFloors'],$_GET['pillars'],$_GET['neighborhood'],$_GET['area'],$_GET['roomsNum'],$_GET['size'],$_GET['groundFloor'],$_GET['bars'],$_GET['pandor'],$_GET['mmd'],$_GET['furnished'],$_GET['parking'],$_GET['pets'],$_GET['garage'],$_GET['partners'],$_GET['airConditioner'],$_GET['invalids'],$_GET['longTime'],$_GET['porch'],$_GET['renovated'],$_GET['exclusivity'],$_GET['price'],$_GET['entryDate'],$_GET['img1'],$_GET['img2'],$_GET['img3'],$_GET['movie'],$_GET['awnerID']);
+    //   addAd($_GET['appartmentType']);
+      }
+     
+?>
+
+ <div class="flex-row justify-space-between margin-5">
+      <div class="yad2-logo icon-min-mid-size"></div>
+      <div class="flex-row align-center  ">
+           <div class="loginIcon icon-min-small-size "></div>
+           <div class="orange margin-4 white-space-nowrap ">מיאל מסוארי</div>
+           <div class="margin-4 white-space-nowrap">צור קשר</div>
+      </div>
+  </div>
+
+<div class="orange text-align-center font-size-35">סיימת!</div>
 
 <?php
 
-   require_once('../../includes/htmlEnd.php');
+    require_once('C:\wamp64\www\firstphp\yad2\includes\htmlEnd.php');
 ?>
